2cdbf98969ed28015de89bfa3777518f0fd90c0e
Susana Varela <104658697+susanavarela@users.noreply.github.com>
2024-07-22 08:18:45 -0300

    Add files via upload

---
+++ transfer/src/main/java/com/transferFile/TransferFileApplication.java
@@ -0,0 +1,13 @@
+package com.transferFile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransferFileApplication.class, args);
+	}
+
+}
---
+++ transfer/src/test/java/com/transferFile/TransferFileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transferFile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferFileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
